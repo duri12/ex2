86f8865ec9b4580090bfa255f98135204d8c5998
duri12 <eyalgor102@gmail.com>
2022-05-08 17:59:48 +0300

    created player.h

--- Player.h
+++ Player.h
@@ -1,4 +1,20 @@
 #ifndef EX2_PLAYER_H
 #define EX2_PLAYER_H
 
+class Player {
+    std::string m_name;
+    int m_level;
+    int m_force;
+    int m_MaxHP;
+    int m_HP;
+    int m_coins;
+
+
+public:
+    Player(std::string name, int maxHP =100, int force =5);
+
+
+};
+
+
 #endif //EX2_PLAYER_H
