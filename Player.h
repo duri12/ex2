b97235e7112dcba72ca45ad0b82b71e3aec740ee
duri12 <eyalgor102@gmail.com>
2022-05-08 18:23:57 +0300

    finished player.h

--- Player.h
+++ Player.h
@@ -14,6 +14,17 @@ public:
     Player(std::string name, int maxHP =100, int force =5);
     Player(const Player&);
     Player operator=(const Player&);
+    ~Player() = default;
+    void printInfo() const;
+    void levelUpdate();
+    int getLevel() const;
+    void buff(int addedForce);
+    void heal(int health);
+    void damage(int damage);
+    bool isKnockedOut() const;
+    void addCoins(int amount);
+    bool pay(int amount);
+    int getAttackStrength() const;
 
 };
 
