7f960d62d4a2e34f1912b65934e4a3802cef6d9c
duri12 <eyalgor102@gmail.com>
2022-05-08 19:49:08 +0300

    finished player.cpp

--- Player.cpp
+++ Player.cpp
@@ -70,3 +70,25 @@ void Player::heal(int amount) {
         }
     }
 }
+
+void Player::damage(int amount) {
+    if(amount >0){
+        if(this->m_HP - amount > 0){
+            this->m_HP -= amount;
+        }
+        else{
+            this->m_HP = 0;
+        }
+    }
+}
+
+
+void Player::addCoins(int amount) {
+    if(amount >0){
+        this->m_coins += amount;
+    }
+}
+
+int Player::getAttackStrength() const{
+    return this->m_level+this->m_force;
+}
