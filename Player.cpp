1b7809ba1f677a7aed5555bd2e9682a1443ddbe3
harelkrelbaum <harelk@campus.technion.ac.il>
2022-05-08 18:31:01 +0300

    ahhhhhh

--- Player.cpp
+++ Player.cpp
@@ -1,10 +1,9 @@
 //
 // Created by harel on 08/05/2022.
 //
-Player operator=(const Player&) {
-    this->m_level =
-    int m_level;
-    int m_force;
+Player operator=(const Player& p2) {
+    this->m_level = p2->m_level;
+    this->m_force = p2->m_force;
     int m_MaxHP;
     int m_HP;
     int m_coins
